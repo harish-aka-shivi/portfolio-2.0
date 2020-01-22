0b9816a77da9a5eac3fdde55da544102941084fe
Harish Rana <r.shivi@yahoo.com>
2020-01-22 23:18:48 +0530

    Updates

--- precache-manifest.41663444ecfac90ff6547682ed069f6a.js
+++ precache-manifest.41663444ecfac90ff6547682ed069f6a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "90c3e6365c0f413f49abb891c4ced92d",
+    "revision": "7db8f7a32e969265454f9491c5416b12",
     "url": "/portfolio-2.0/index.html"
   },
   {
-    "revision": "27ad265bf0221d7982a9",
+    "revision": "ddfa16da03d3ff5eccd6",
     "url": "/portfolio-2.0/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "1a18982cf59aeca8244e",
-    "url": "/portfolio-2.0/static/js/2.0ebe6436.chunk.js"
+    "revision": "0944f44fcaa7db68dcbd",
+    "url": "/portfolio-2.0/static/js/2.82c644ff.chunk.js"
   },
   {
-    "revision": "27ad265bf0221d7982a9",
-    "url": "/portfolio-2.0/static/js/main.2db7ab31.chunk.js"
+    "revision": "ddfa16da03d3ff5eccd6",
+    "url": "/portfolio-2.0/static/js/main.2a35f122.chunk.js"
   },
   {
     "revision": "0025e6b95168707d8a31",
@@ -22,6 +22,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "140dc054735460d718d7a641b977e111",
     "url": "/portfolio-2.0/static/media/account.140dc054.svg"
+  },
+  {
+    "revision": "fb805a09cedc7c53cc498384a846f3df",
+    "url": "/portfolio-2.0/static/media/alert.fb805a09.svg"
   },
   {
     "revision": "8a806979507e88c31077b6d9fbbcedf3",
@@ -34,6 +38,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "60093be5802c82ef0a58d97f5a377d3a",
     "url": "/portfolio-2.0/static/media/gmail.60093be5.svg"
+  },
+  {
+    "revision": "d9e7c64a889ca0140dab0fe28aca6245",
+    "url": "/portfolio-2.0/static/media/headshot.d9e7c64a.jpeg"
   },
   {
     "revision": "f619cdc0fb7aae06d61a7a582a767282",
@@ -70,6 +78,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6c15e1259464e5f097bc8f32f848294d",
     "url": "/portfolio-2.0/static/media/twitter.6c15e125.svg"
+  },
+  {
+    "revision": "9a7cd532a7e57ec11f9de0e860a5e8d7",
+    "url": "/portfolio-2.0/static/media/vistoso.9a7cd532.png"
+  },
+  {
+    "revision": "944161f41ca018af3a5b82c06fc11a0d",
+    "url": "/portfolio-2.0/static/media/windows.944161f4.svg"
   },
   {
     "revision": "bc7806c091442a19f9ff9961f4f38374",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-2.0/precache-manifest.f0e794351ad2b1a64735b310973c0a84.js"
+  "/portfolio-2.0/precache-manifest.41663444ecfac90ff6547682ed069f6a.js"
 );
 
 self.addEventListener('message', (event) => {
