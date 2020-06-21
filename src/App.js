5a18f7f1880c387b4f096b6f5590302232283c7a
Harish Rana <r.shivi@yahoo.com>
2020-06-21 13:13:17 +0530

    feat: add resume route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Desktop from './containers/windows/desktop';
+import ResumeRedirect from './pages/resume';
+
+const AppRouter = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/resume">
+        <ResumeRedirect />
+      </Route>
+      <Route path="/">
+        <Desktop />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 function App() {
   return (
     <main>
-      <Desktop />
+      <AppRouter />
     </main>
   );
 }
